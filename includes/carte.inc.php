ad8eec5f8598c78e93961f614f9304c85a83c071
Sonia Chalal <contact@soniawebdesigner.fr>
2022-11-17 00:01:45 +0100

    Finir la page carte

--- includes/carte.inc.php
+++ includes/carte.inc.php
@@ -2,8 +2,8 @@
 <div class="carte">
 
         <div class="carte_titre">
-            <h2>Carte menu</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, eum!</p>
+            
+            <p><span>YMN</span> vous propose une oasis de saveurs avec des plats issues de la cuisine marocaine orientale.</p>
         </div>
         <div class="category">
             <div class="swiper mySwiper filters d-flex col">
@@ -20,7 +20,8 @@
                                     
                                     <div class="filters swiper-wrapper target" id="<?php echo $value['dataFilter_categorie'];?>" >
                                 
-                                    <a  id="<?php echo $value['dataFilter_categorie'];?>" class="active" name="<?php echo $value['dataFilter_categorie'];?>" href="?dataFilter=<?php echo $value['dataFilter_categorie']; ?>#recipes">
+                                    <a  id="<?php echo $value['dataFilter_categorie'];?>" class="active" name="<?php echo $value['dataFilter_categorie'];?>" 
+                                    href="?page=carte&dataFilter=<?php echo $value['dataFilter_categorie']; ?>&nom_categorie=<?php echo $value['nom_categorie'];?>">
                                         <?php echo $value['nom_categorie'];?>
                                     </a>
                                     </div>
@@ -36,21 +37,29 @@
         </div>
         <div class="container">
        
-        <?php   
+        <?php    
+       
                     if(!isset($_GET['dataFilter']))
                     {
                         $AlldataFilter = "AllProduct";
-                         $reqSelectProduits = "SELECT * FROM produits, categorie WHERE categorie.dataFilter_categorie ='". $AlldataFilter ."'";
+                        $reqSelectProduits = "SELECT titre_produit, url_produit, categorie.nom_categorie, prix_produit, description_produit, dataFilter_produit 
+                        FROM produits, categorie 
+                        WHERE categorie.dataFilter_categorie ='". $AlldataFilter ."'";
                     }
                     if(isset($_GET['dataFilter']) && $_GET['dataFilter']=="AllProduct")
-                    {
-                        $reqSelectProduits = "SELECT * FROM produits";   
+                    {   
+                        $AlldataFilter = "AllProduct";
+                        $reqSelectProduits = "SELECT titre_produit, url_produit, categorie.nom_categorie, prix_produit, description_produit, dataFilter_produit  
+                        FROM produits, categorie 
+                        WHERE categorie.dataFilter_categorie ='". $AlldataFilter ."'";
                     }
                     if(isset($_GET['dataFilter']) && $_GET['dataFilter']!="AllProduct")
                     {
                         $filterCategorie = $_GET['dataFilter'];
-                        $reqSelectProduits = "SELECT * FROM produits, categorie WHERE dataFilter_produit ='". $filterCategorie ."'";
-                        
+                        $nomCategorie = $_GET['nom_categorie'];
+                        $reqSelectProduits = "SELECT titre_produit, url_produit, categorie.nom_categorie, prix_produit, description_produit, dataFilter_produit 
+                        FROM produits, categorie 
+                        WHERE categorie.nom_categorie ='". $nomCategorie ."' AND dataFilter_produit ='". $filterCategorie ."'";
                     }
                     $cnx = new sql();
                     $reqSelectProduits = $cnx->afficher($reqSelectProduits);
@@ -59,7 +68,6 @@
                     foreach ($reqSelectProduits as $key => $value) 
                     {
                     ?> 
-                    
                     <div class="box">
                         <div class="item">
                             <p><h3><?php echo $value['titre_produit']; ?></h3></p>
@@ -69,24 +77,23 @@
                                 </figure>
                             </div>
                             <div class="carte-content">
-                            <div class="card-meta">
-		                        <p class="dish-type"><?php echo $value['nom_categorie']; ?></p>
-                                    <ul class="dish-stats">
-                                        <li>
-                                        <h5><span><?php echo $value['prix_produit']; ?> €</span></h5>
-                                        </li>
-                                        <li>
-                                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.9713 2.5C7.00005 2.5 8.00005 4.5 8.00005 4.5C8.00005 4.5 9.00005 2.5 11.0288 2.5C12.6775 2.5 13.9832 3.87937 14 5.52531C14.0344 8.94187 11.2897 11.3716 8.2813 13.4134C8.19836 13.4699 8.10036 13.5 8.00005 13.5C7.89973 13.5 7.80174 13.4699 7.7188 13.4134C4.71067 11.3716 1.96598 8.94187 2.00005 5.52531C2.01692 3.87937 3.32255 2.5 4.9713 2.5Z" stroke-linecap="round" stroke-linejoin="round"/>
-                                            </svg>
-                                            237
-                                        </li>
-                                    </ul>
-	                        </div> <!-- end card-meta -->
-                            
-                                <p><?php echo $value['description_produit']; ?></p>
-                            </div>
-                            <div class="button btn">Voir la carte</div>
+                                <div class="card-meta">
+                                    <p class="dish-type"><?php echo $value['dataFilter_produit']; ?></p>
+                                        <ul class="dish-stats">
+                                            <li>
+                                            <h5><span class="prix"><?php echo $value['prix_produit']; ?> €</span></h5>
+                                            </li>
+                                            <li>
+                                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M4.9713 2.5C7.00005 2.5 8.00005 4.5 8.00005 4.5C8.00005 4.5 9.00005 2.5 11.0288 2.5C12.6775 2.5 13.9832 3.87937 14 5.52531C14.0344 8.94187 11.2897 11.3716 8.2813 13.4134C8.19836 13.4699 8.10036 13.5 8.00005 13.5C7.89973 13.5 7.80174 13.4699 7.7188 13.4134C4.71067 11.3716 1.96598 8.94187 2.00005 5.52531C2.01692 3.87937 3.32255 2.5 4.9713 2.5Z" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </svg>
+                                                237
+                                            </li>
+                                        </ul>
+                                       
+                                </div> <!-- end card-meta -->
+                            </div> <p><?php echo $value['description_produit']; ?></p>
+                            <div class="button btn">Ajouter +</div>
                         </div>
                     </div>
                 <?php
--- includes/nav.php
+++ includes/nav.php
@@ -1,11 +1,12 @@
 <nav class="navbar dark-mode" role="navigation">
     <div class="row d-flex container">
         <div class="navbar__logo"><a href="index.php?page=accueil"><img
-                    src="./assets/images/logo/logoYMNsansfond245.png" alt="YNM - Restaurant oriental - Oissel"></a>
+                    src="./assets/images/logo/logoJaune/logoYMNJaune.png" alt="YNM - Restaurant oriental - Oissel"></a>
+                    <h2><strong><i class="fa-solid fa-phone"></i> 06 13 40 45 87</strong></h2>
         </div>
         <ul class="navbar navbar__links nav-list d-flex">
             <li class="navbar__link first"><a href="index.php?page=accueil">Accueil</a></li>
-            <li class="navbar__link second"><a href="index.php?page=carte">La carte</a></li>
+            <li class="navbar__link second"><a href="index.php?page=carte">Notre carte</a></li>
             <li class="navbar__link third"><a href="index.php?page=evenement">Evenements</a></li>
             <li class="navbar__link four"><a href="index.php?page=apropos">À propos</a></li>
             <li class="navbar__link five"><a href="index.php?page=ajoutEvenement">Ajouter un event</a></li>
