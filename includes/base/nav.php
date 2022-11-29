c8241348c0015a153d49621a62f78e623513992b
Sonia Chalal <contact@soniawebdesigner.fr>
2022-11-29 23:50:13 +0100

    Création de la page espace d'un utilisateur qui passe une commande

--- includes/base/nav.php
+++ includes/base/nav.php
@@ -4,20 +4,29 @@
         <div class="navbar__logo"><a href="index.php?page=accueil">
             <img
             src="./assets/images/favicon/android-icon-192x192.png" alt="YNM - Restaurant oriental - Oissel"></a>
-            <h2><strong><i class="fa-solid fa-phone"></i> 06 13 40 45 87</strong></h2>
+            <h4><strong><i class="fa-solid fa-phone"></i> 06 13 40 45 87</strong></h4>
         </div>
         <ul class="navbar navbar__links nav-list d-flex">
             <li class="navbar__link"><a href="index.php?page=accueil">Accueil</a></li>
             <li class="navbar__link"><a href="index.php?page=carte">La carte</a></li>
             <li class="navbar__link"><a href="index.php?page=evenement">Listes des Evenements</a></li>
             <li class="navbar__link"><a href="index.php?page=apropos">À propos</a></li>
+            
         <?php
         if (isset($_SESSION['username'])) {
             ?>
-                <span class="username"> <i class="fa fa-user monCompte" aria-hidden="true"></i><?php echo $_SESSION["username"] ?></span>
+                <span class="cart-ico username">
+                    <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+                    <span class="panier">0</span>
+                </span>
+                <span class="username">
+                    <i class="fa fa-user monCompte" aria-hidden="true"></i>
+                    <?php echo $_SESSION["username"] ?>
+                </span>
                 <li class="navbar__link">
                     <a href="index.php?page=logout" class="btn signin">
-                        <span><i class="fa fa-sign-out" aria-hidden="true"></i>Déconnexion
+                        <span><i class="fa fa-sign-out" aria-hidden="true"></i>
+                        Déconnexion
                     </a></span>
                 </li>
             <?php
@@ -29,16 +38,7 @@
             <button class="burger" onclick="show()">
                 <span class="bar"></span>
             </button>
-            <div class="col d-flex">
-                <form>
-                    <input type="search" placeholder="Search your item" />
-                    <button class="d-flex"><i class="bx bx-search"></i></button>
-                </form>
-                <div class="cart-icon d-flex">
-                    <i class="bx bx-shopping-bag"></i>
-                    <span>0</span>
-                </div>
-            </div> 
+           
             <!-- Hamburger -->
             <div class="hamburger d-flex">
                 <i class="bx bx-menu"></i>
--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,25 +1,62 @@
 <?php
 session_start();
+
 if($_SERVER["REQUEST_METHOD"]=="POST")
 {
     $username = $_POST["username"];
     $password = $_POST["password"];
+    $erreurs = array();
+
+    if (mb_strlen($username) === 0){
+        array_push($erreurs, "Il manque votre e-mail");
+        
+    }
+    if (mb_strlen($password) === 0){
+        array_push($erreurs, "Il manque votre mot de passe");
+        
+    }
+    if (count($erreurs)) {
+        $messageErreur = "<ul>";
+        for ($i = 0; $i < count($erreurs); $i++) 
+        {
+            $messageErreur .= "<li>";
+            $messageErreur .= $erreurs[$i];
+            $messageErreur .= "</li>";
+        }
+        $messageErreur .= "</ul>";
+        echo $messageErreur;
+        header ('location: /index.php?page=accueil');
+    }
+
 
     $requeteLogin="SELECT * FROM utilisateur WHERE username='".$username."' AND password='".$password."'";
     $sqlLogin = new Sql();
     $resultatLogin = $sqlLogin->afficher($requeteLogin);
     
-    if (count($resultatLogin) > 0) {
+    if (count($resultatLogin) > 0) 
+    {
         if($resultatLogin[0]['role']=="user")
         { 
             $_SESSION["username"] = $username; 
-            header('location: /index.php?page=accueil');
+            header('location: admin/index.php?page=espaceUser');
         }
         elseif($resultatLogin[0]['role']=="admin")
         {
             $_SESSION["username"] = $username; 
             header('Location: admin/index.php?page=accueil');
         }
+        else
+        {
+          echo  $message = "Erreur d'authentification";
+            $_SESSION['username'] = false;
+        }
+    }elseif(count($resultatLogin) == 0 ){
+       echo $message = "Erreur d'authentification";
+        $_SESSION["username"] = "";
+        session_destroy();
+        header ('location:http://localhost/newNormayna/index.php');
+
     }
+
 }
 ?>
